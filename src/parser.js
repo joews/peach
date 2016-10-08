2383823bd0dcfa6136d9ccb7cc755f9ffea57f93
Joe Whitfield-Seed <joeseed86@gmail.com>
2016-10-08 21:34:39 +0100

    Add browserify to build browser bundles
    brfs finds fs.readFileSync calls statically so I had to move the parser
    path inline.

--- src/parser.js
+++ src/parser.js
@@ -3,10 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const peg = require('pegjs')
 
-// TODO cache the compiled parser
-const parserPath = path.join(__dirname, 'peach.pegjs')
-
-const parserSource = fs.readFileSync(parserPath, 'utf8')
+const parserSource = fs.readFileSync(path.join(__dirname, 'peach.pegjs'), 'utf8')
 const parser = peg.generate(parserSource)
 
 module.exports = function parse (source) {
